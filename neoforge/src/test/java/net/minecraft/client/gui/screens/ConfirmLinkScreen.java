cb398cda8ff3e177439c1a25f21394e7079e9b73
Ashley <1834870+ashleywr@users.noreply.github.com>
2026-06-02 14:30:28 +0900

    Prepare 1.0.0 release

---
+++ neoforge/src/test/java/net/minecraft/client/gui/screens/ConfirmLinkScreen.java
@@ -0,0 +1,10 @@
+package net.minecraft.client.gui.screens;
+
+import it.unimi.dsi.fastutil.booleans.BooleanConsumer;
+import net.minecraft.network.chat.Component;
+
+public class ConfirmLinkScreen extends Screen {
+    public ConfirmLinkScreen(BooleanConsumer callback, Component title, Component message,
+                             String url, Component noButton, boolean trusted) {
+    }
+}
